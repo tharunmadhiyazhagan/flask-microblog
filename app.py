d52233fc3d6b73032af5dfa5113f2c67ddcefb1b
tharunmadhiyazhagan <146400645+tharunmadhiyazhagan@users.noreply.github.com>
2023-10-03 19:11:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,26 +8,45 @@ load_dotenv()
 
 def create_app():
   app = Flask(__name__)
-  client = MongoClient(os.getenv("MONGODB_URI"))
-  app.db = client.microblog2
+  try:
+    client = MongoClient(os.getenv("MONGODB_URI"))
+  except Exception as e:
+    print("Error connecting to MongoDB:", str(e))
+
+  # client = MongoClient(os.getenv("MONGODB_URI"))
+  app.db = client.microblog2  
 
   @app.route("/", methods=["GET", "POST"])
   def home():
+    if request.method == "GET":
+        # Check if there are any entries in the database
+        num_entries = app.db.entries.count_documents({})
+
+        if num_entries > 0:
+            # If there are entries, create the list
+            entries_with_date = [
+                (entry["content"], entry["date"], datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"))
+                for entry in app.db.entries.find({})
+            ]
+        else:
+            # If there are no entries, set entries_with_date to an empty list
+            entries_with_date = []
+
     if request.method == "POST":
       entry_content = request.form.get("content")
       formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
       app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
-    entries_with_date = [
-      (entry["content"], entry["date"], datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"))
-      for entry in app.db.entries.find({})
-    ]
+    # entries_with_date = [
+    #   (entry["content"], entry["date"], datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"))
+    #   for entry in app.db.entries.find({})
+    # ]
     return render_template("home.html", entries=entries_with_date)
-  return app
+  return app   
 
 if __name__ == "__main__":
   app = create_app()
-  app.run()
+  app.run(host="0.0.0.0", port=5000)
 
 
 
